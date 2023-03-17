117358b0c1a88ce4160b5c753cdd9f785c4e07d4
Stolbin <109080579+Stolbin@users.noreply.github.com>
2023-03-17 13:26:43 +0300

    upd

--- js/anchor.js
+++ js/anchor.js
@@ -1,52 +1,52 @@
-var $page = $('html, body');
+var $page = $("html, body");
 $('a[href*="#home"]').click(function () {
   $page.animate(
     {
-      scrollTop: $($.attr(this, 'href')).offset().top,
+      scrollTop: $($.attr(this, "href")).offset().top,
     },
     300
   );
   return false;
 });
 
-var $page = $('html, body');
+var $page = $("html, body");
 $('a[href*="#about"]').click(function () {
   $page.animate(
     {
-      scrollTop: $($.attr(this, 'href')).offset().top,
+      scrollTop: $($.attr(this, "href")).offset().top,
     },
     300
   );
   return false;
 });
 
-var $page = $('html, body');
-$('a[href*="#product"]').click(function () {
+var $page = $("html, body");
+$('a[href*="#services"]').click(function () {
   $page.animate(
     {
-      scrollTop: $($.attr(this, 'href')).offset().top,
+      scrollTop: $($.attr(this, "href")).offset().top,
     },
     300
   );
   return false;
 });
 
-var $page = $('html, body');
+var $page = $("html, body");
 $('a[href*="#markets"]').click(function () {
   $page.animate(
     {
-      scrollTop: $($.attr(this, 'href')).offset().top,
+      scrollTop: $($.attr(this, "href")).offset().top,
     },
     300
   );
   return false;
 });
 
-var $page = $('html, body');
-$('a[href*="#footer"]').click(function () {
+var $page = $("html, body");
+$('a[href*="#contacts"]').click(function () {
   $page.animate(
     {
-      scrollTop: $($.attr(this, 'href')).offset().top,
+      scrollTop: $($.attr(this, "href")).offset().top,
     },
     300
   );
